0f5a08458b86b1045a20ac3a0cd8c431ceef9ca1
Vitalii Zakharov <lewiss945@gmail.com>
2018-03-23 20:37:32 +0200

    Add default projects. + Added gitignore.

---
+++ PortableLibraryTelegramBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PortableLibraryTelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
