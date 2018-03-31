374cb85c07ba7b178899314bb3807f19b936fe97
Vitalii Zakharov <lewiss945@gmail.com>
2018-04-01 00:45:40 +0300

    Added litres and lostfilm test classes.

---
+++ Tests/PortableLibraryCoreTests/Infrastructure/Services/Book/LitresExternalProviderTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortableLibraryCoreTests.Infrastructure.Services.Book
+{
+    public class LitresExternalProviderTests
+    {
+        [Fact]
+        public async Task Should()
+        {
+        }
+    }
+}
---
+++ Tests/PortableLibraryCoreTests/Infrastructure/Services/TvShow/LostFilmExternalProviderTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortableLibraryCoreTests.Infrastructure.Services.TvShow
+{
+    public class LostFilmExternalProviderTests
+    {
+        [Fact]
+        public async Task Should()
+        {
+        }
+    }
+}
