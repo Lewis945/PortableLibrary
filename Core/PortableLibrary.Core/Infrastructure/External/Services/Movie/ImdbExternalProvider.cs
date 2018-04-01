3877a7e451fc5634090c7571a47b50e6c2931544
Vitalii Zakharov <lewiss945@gmail.com>
2018-04-01 19:11:38 +0300

    Added imdb and myshows external providers.

---
+++ Core/PortableLibrary.Core/Infrastructure/External/Services/Movie/ImdbExternalProvider.cs
@@ -0,0 +1,14 @@
+﻿using PortableLibrary.Core.External.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortableLibrary.Core.Infrastructure.External.Services.Movie
+{
+    /// <summary>
+    /// http://www.imdb.com
+    /// </summary>
+    public class ImdbExternalProvider : IExternalServiceProvider
+    {
+    }
+}
