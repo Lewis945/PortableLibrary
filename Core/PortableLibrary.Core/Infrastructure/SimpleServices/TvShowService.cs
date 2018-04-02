e99e7e61bb9ee05dd7540e1fce36529339e4c5c4
Vitalii Zakharov <lewiss945@gmail.com>
2018-04-02 18:01:32 +0300

    Added add tv show method.

--- Core/PortableLibrary.Core/Infrastructure/SimpleServices/TvShowService.cs
+++ Core/PortableLibrary.Core/Infrastructure/SimpleServices/TvShowService.cs
@@ -1,4 +1,7 @@
-﻿using PortableLibrary.Core.Database;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PortableLibrary.Core.Database;
 using PortableLibrary.Core.SimpleServices;
 
 namespace PortableLibrary.Core.Infrastructure.SimpleServices
@@ -22,7 +25,38 @@ namespace PortableLibrary.Core.Infrastructure.SimpleServices
 
         #region ITvShowService
 
-        
+        public async Task<bool> AddLibraryTvShowAsync(string tvShowName, string author, string libraryName)
+        {
+            try
+            {
+                var library =
+                    await _context.BookLibraries.FirstOrDefaultAsync(l => !l.IsDeleted && l.Name == libraryName);
+
+                if (library == null)
+                    return false;
+
+                var libraryBook = library.Books.FirstOrDefault(b => b.Name == bookName && b.Author == author);
+
+                if (libraryBook != null)
+                    return false;
+
+                libraryBook = new LibraryBook
+                {
+                    Name = bookName,
+                    Author = author
+                };
+
+                library.Books.Add(libraryBook);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
 
         #endregion
 
