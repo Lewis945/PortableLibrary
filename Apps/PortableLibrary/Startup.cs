faeba1c9bd5ac27120a802ff254c2156ff936151
Vitalii Zakharov <lewiss945@gmail.com>
2018-06-10 22:49:00 +0300

    Minor project updates.

--- Apps/PortableLibrary/Startup.cs
+++ Apps/PortableLibrary/Startup.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using PortableLibrary.Core.Database;
+using PortableLibrary.Core.Infrastructure.SimpleServices;
+using PortableLibrary.Core.SimpleServices;
 
 namespace PortableLibrary
 {
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-dotnet-sqldatabase
+    /// https://docs.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-dotnetcore-sqldb
+    /// </summary>
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -23,7 +31,15 @@ namespace PortableLibrary
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<PortableLibraryDataContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ILibraryService, LibraryService>();
+
             services.AddMvc();
+
+            // Secrets usage
+            // https://blogs.msdn.microsoft.com/mihansen/2017/09/10/managing-secrets-in-net-core-2-0-apps/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
