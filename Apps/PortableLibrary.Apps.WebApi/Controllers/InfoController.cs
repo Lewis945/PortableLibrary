26b3cfd727b09e5119137a9a9716dee7a3173849
Vitalii Zakharov <lewiss945@gmail.com>
2018-10-08 00:41:29 +0300

    Added info controller.

---
+++ Apps/PortableLibrary.Apps.WebApi/Controllers/InfoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortableLibrary.Core.Database;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PortableLibrary.Apps.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InfoController : ControllerBase
+    {
+        private PortableLibraryDataContext _dataContext;
+
+        public InfoController(PortableLibraryDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Version()
+        {
+            return "1.0";
+        }
+
+        [HttpGet]
+        public ActionResult<string> Author()
+        {
+            return "Vitalii Zakharov";
+        }
+
+        [HttpGet]
+        public ActionResult<List<string>> PublicLibraries()
+        {
+            return _dataContext.BookLibraries.Select(l => l.Name).ToList();
+        }
+    }
+}
