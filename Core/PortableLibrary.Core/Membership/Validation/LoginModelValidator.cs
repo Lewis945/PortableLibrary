3c2b288937fa1d0739a5c28c3dd060e07a862520
Vitalii Zakharov <lewiss945@gmail.com>
2018-10-17 20:39:32 +0300

    Added membership models validators.

---
+++ Core/PortableLibrary.Core/Membership/Validation/LoginModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using PortableLibrary.Core.Membership.Models;
+
+namespace PortableLibrary.Core.Membership.Validation
+{
+    public class LoginModelValidator : AbstractValidator<LoginModel>
+    {
+        public LoginModelValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).Length(6, 12);
+        }
+    }
+}
---
+++ Core/PortableLibrary.Core/Membership/Validation/RegisterModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using PortableLibrary.Core.Infrastructure.Models;
+
+namespace PortableLibrary.Core.Membership.Validation
+{
+    public class RegisterModelValidator : AbstractValidator<RegisterModel>
+    {
+        public RegisterModelValidator()
+        {
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Password).Length(6, 12);
+            RuleFor(x => x.ConfirmPassword).Matches(x => x.Password);
+        }
+    }
+}
